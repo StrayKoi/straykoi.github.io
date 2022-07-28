450b480d0554002dcfb6e87a17e8578b82373f69
Stray Koi <barberry.koi@gmail.com>
2022-07-27 22:58:30 -0400

    Site launch

---
+++ scripts.js
@@ -0,0 +1,19 @@
+// Back to top script
+var backtotop = document.getElementById("backtop");
+
+// When the user scrolls down 25px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 25 || document.documentElement.scrollTop > 25) {
+    backtotop.style.display = "block";
+  } else {
+    backtotop.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
